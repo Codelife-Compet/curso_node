2f3982c94a78be90c0dae53be04b7ff183208ce8
Henrique de Paula <tec.henriquedepaula@gmail.com>
2022-06-03 13:48:32 -0300

    Adicionando Página de biscoito da sorte

--- Capítulo_3/codelife.js
+++ Capítulo_3/codelife.js
@@ -1,29 +1,52 @@
+const e = require('express')
 const express = require('express')
 const expressHandlebars = require('express-handlebars') 
 const app = express()
 const port = process.env.PORT || 3000
-
-const engineHandlebars = expressHandlebars.engine({
-    defaultLayout:'main'
+const engineHandlebars = expressHandlebars.engine({defaultLayout:'main'})
+const fortuneCookies = [
+    "Domine Nodejs ou o NodeJs te dominará",
+    "Deus ajuda quem programa na madruga",
+    "Hoje você deveria escutar a oração do programador",
+    "Hoje você terá que criar uma issue no nosso repositório",
+    "Hoje você vai ter que criar uma página e renderizar ela usando express",
+    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
+    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
+    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
+    "Hoje você vai subir o código para o github",
+    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
+    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
+    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
+    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
+    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+    "Hoje você vai ter que explicar uma página do código",
+]
+app.engine('handlebars', engineHandlebars)
+app.set('view engine','handlebars')
+app.get('/cookieSort', (req, res) => {
+    const randomFortune=fortuneCookies[Math.floor(Math.random()*fortuneCookies.length)]
+    res.render('cookieSort',{fortune:randomFortune})
 })
-app.engine('handlebars',engineHandlebars)
 app.get('/', (req, res) => {
-    res.type('text/plain')
     res.render('home')
 })
 app.get('/about', (req, res) => {
-    res.type('text/plain')
     res.render('about')
 })
 app.use(express.static(__dirname+'/public'))
 app.use((err, req, res, next) => {
-    res.type('text/plain')
+    console.error(err)
     res.status(404)
     res.render('404')
 })
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
     res.render('500')
 })
