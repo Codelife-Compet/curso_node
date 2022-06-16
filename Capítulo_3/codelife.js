26705b1eaae47b1554691d2c1ce3738eaefce7b5
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-16 18:25:26 -0300

    Adicionando alguns biscoitos da sorte

--- Capítulo_3/codelife.js
+++ Capítulo_3/codelife.js
@@ -1,55 +1,53 @@
-const e = require('express')
-const express = require('express')
-const expressHandlebars = require('express-handlebars') 
-const app = express()
-const port = process.env.PORT || 3000
-const engineHandlebars = expressHandlebars.engine({defaultLayout:'main'})
-const fortuneCookies = [
-    "Domine Nodejs ou o NodeJs te dominará",
-    "Deus ajuda quem programa na madruga",
-    "Hoje você deveria escutar a oração do programador",
-    "Hoje você terá que criar uma issue no nosso repositório",
-    "Hoje você vai ter que criar uma página e renderizar ela usando express",
-    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
-    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
-    "Hoje você vai ter que adicionar um arquivo CSS ao nosso documento",
-    "Hoje você vai subir o código para o github",
-    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
-    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
-    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
-    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
-    "Hoje você vai ter que escolher alguém pra explicar uma parte do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-    "Hoje você vai ter que explicar uma página do código",
-]
-app.engine('handlebars', engineHandlebars)
-app.set('view engine','handlebars')
-app.get('/cookieSort', (req, res) => {
-    const randomFortune=fortuneCookies[Math.floor(Math.random()*fortuneCookies.length)]
-    res.render('cookieSort',{fortune:randomFortune})
+const express = require('express');
+const expressHandlebars = require('express-handlebars'); 
+const app = express();
+const port = process.env.PORT || 3000;
+
+const engineHandlebars = expressHandlebars.engine({
+    defaultLayout:'main'
 })
+app.engine('handlebars', engineHandlebars);
+app.set('view engine', 'handlebars');
+const fortunes = [
+    "Crie uma nova página do zero",
+    "Edite uma parte do código e teste",
+    "Crie um arquivo CSS",
+    "Vincule um arquivo CSS ao projeto, se não existir crie um",
+    "Adicione uma nova tarefa para o projeto",
+    "Escolha alguém para explicar uma parte do código",
+    "Explique uma parte do código"
+];
+const codelifers = [
+    "Henrique",
+    "Richard",
+    "Natália",
+    "Lucas",
+    "Francisco"
+];
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.render('home')
-})
+    res.render('home');
+});
+app.set('views', __dirname+'/views');
+
+app.get('/fortune', (req, res) => {
+    const codelifer = codelifers[Math.floor(Math.random() * codelifers.length)];
+    const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('cookieSort',{codelifer:codelifer,fortune:fortune});
+});
 app.get('/about', (req, res) => {
-    res.render('about')
-})
-app.use(express.static(__dirname+'/public'))
+    res.render('about');
+});
 app.use((err, req, res, next) => {
-    console.error(err)
-    res.status(404)
-    res.render('404')
-})
+    res.status(404);
+    res.render('404');
+});
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
+});
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
