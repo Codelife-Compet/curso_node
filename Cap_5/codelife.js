b034394228bc7636412da9cd55b9b8ae8a6dc934
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-17 09:49:05 -0300

    Migrando pasta public e views para a raíz do projeto

--- Cap_5/codelife.js
+++ Cap_5/codelife.js
@@ -10,8 +10,8 @@ const engineHandlebars = expressHandlebars.engine({
 // Configurações do app como referência de arquivos estáticos, view engine e etc...
 app.engine("handlebars", engineHandlebars);
 app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
 // Servindo as rotas configuradas anteriormente através dos handlers definidos no capítulo atual
 app.get("/", handlers.home);
 app.get("/about", handlers.about);
