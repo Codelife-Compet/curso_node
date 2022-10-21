04c82321116f7373ecaec7700e60d550f592c49c
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-21 20:52:24 -0300

    Caso o tour que se deseja adicionar já exista no banco dedados deverá retornar um erro

--- Cap_6/lib/__tests__/postApi.test.js
+++ Cap_6/lib/__tests__/postApi.test.js
@@ -1,4 +1,5 @@
 const { Tour } = require("../../api/model/tour");
+const { tourController } = require("../../api/tours");
 describe("postApi", () => {
   const setData = (data) => {
     if (!(data instanceof Tour)) {
@@ -6,8 +7,11 @@ describe("postApi", () => {
         "Não foi possível cadastrar o novo tour, entrada inválida"
       );
     }
+    if (tourController.getData().includes(data)) {
+      throw new Error("Tour já existe");
+    }
   };
-  it("O método deve retornar um erro caso o tipo de dado não seja adequado", () => {
+  it("O método deve lançar uma exceção caso o tipo de dado não seja adequado", () => {
     const newData = {};
     const mock = () => {
       setData(newData);
@@ -15,5 +19,12 @@ describe("postApi", () => {
     expect(mock).toThrow(
       new Error("Não foi possível cadastrar o novo tour, entrada inválida")
     );
+  });
+  it("O método deve lançar uma exceção caso o dado já exista no banco de dados", () => {
+    const newData = tourController.getData()[0];
+    const mock = () => {
+      setData(newData);
+    };
+    expect(mock).toThrow(new Error("Tour já existe"));
   });
 });
