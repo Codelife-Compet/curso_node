2c6f11fc36a2975c038da83cb70d681f6b7fd479
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-22 10:27:19 -0300

    Criando método post para o tourController

--- Cap_6/lib/__tests__/postApi.test.js
+++ Cap_6/lib/__tests__/postApi.test.js
@@ -1,5 +1,6 @@
 const { Tour } = require("../../api/model/tour");
 const { tourController } = require("../../api/tours");
+const { postTours } = require("../tourController");
 const data = tourController.getData();
 class TourControllerSpy {
   data;
@@ -23,26 +24,57 @@ class TourControllerSpy {
   };
 }
 const makeTour = () => new TourControllerSpy(data);
+const postTourSpy = (req, res) => {
+  const tour = req.body.tour;
+  const tourControllerSpy = makeTour();
+  try {
+    tourControllerSpy.postData({
+      id: tour.id,
+      name: tour.name,
+      price: tour.price,
+    });
+    res.status(201);
+    res.json({ success: true, createdTour: tour });
+  } catch (error) {
+    console.error(error);
+    res.status(403);
+    res.json({ code: 403, error });
+  }
+};
 describe("postApi", () => {
   it("O método deve lançar uma exceção caso o tipo de dado não seja adequado", () => {
+    // Arrange
+
     const tourControllerSpy = makeTour();
     const newData = {};
+    // Act
     const mock = () => {
       tourControllerSpy.postData(newData);
     };
+    // Assert
     expect(mock).toThrow(
       new Error("Não foi possível cadastrar o novo tour, entrada inválida")
     );
   });
   it("O método deve lançar uma exceção caso o dado já exista no banco de dados", () => {
+    // Arrange
+
     const tourControllerSpy = makeTour();
     const newData = new Tour({ id: 121, name: "Hood River", price: 122.9 });
+
+    // Act
+
     const mock = () => {
       tourControllerSpy.postData({ ...newData });
     };
+
+    // Assert
+
     expect(mock).toThrow(new Error("Tour já existe"));
   });
   it("O Método deve adicionar o novo dado ao banco de dados caso não exista um igual", () => {
+    // Arrange
+
     const newData = new Tour({
       id: 1,
       name: "Boston Medical Group",
@@ -51,12 +83,65 @@ describe("postApi", () => {
     const tourControllerSpy = makeTour();
     const postData = jest.fn(() => tourControllerSpy.postData({ ...newData }));
     const lengthOfData = tourControllerSpy.data.length;
+
+    // Act
+
     postData();
+
+    // Assert
+
     expect(postData.mock.calls.length).toBe(1);
     expect(postData).toHaveReturnedWith(
       expect.objectContaining({
         length: lengthOfData + 1,
         novoTourId: newData.id,
+      })
+    );
+  });
+  it("O usuário receba uma mensagem de erro caso não seja possível cadastra o novo tour", () => {
+    // Arrange
+
+    const req = { body: { tour: { faketour: "fail tour" } } };
+    const res = { status: jest.fn(), json: jest.fn() };
+
+    // Act
+
+    postTourSpy(req, res);
+
+    // Assert
+
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(res.status.mock.calls[0][0]).toBe(403);
+    expect(res.json.mock.calls[0][0]).toEqual(
+      expect.objectContaining({
+        code: 403,
+        error:
+          new Error(
+            "Não foi possível cadastrar o novo tour, entrada inválida"
+          ) || new Error("Tour já existe"),
+      })
+    );
+  });
+  it("O usuário receba uma mensagem em formato json informando sucesso e retornando o tour cadastrado", () => {
+    // Arrange
+
+    const req = {
+      body: { tour: { id: 12, name: "Any Tour name", price: 129.99 } },
+    };
+    const res = { status: jest.fn(), json: jest.fn() };
+
+    // Act
+
+    postTourSpy(req, res);
+
+    // Assert
+
+    expect(res.status.mock.calls.length).toBe(1);
+    expect(res.status.mock.calls[0][0]).toBe(201);
+    expect(res.json.mock.calls[0][0]).toEqual(
+      expect.objectContaining({
+        success: true,
+        createdTour: req.body.tour,
       })
     );
   });
