9ffebd61070eb5e67bf668e0c96f9ed24bb1d308
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-10-21 21:12:38 -0300

    Refatorando teste postApi para implementação baseado em classe

--- Cap_6/api/tours.js
+++ Cap_6/api/tours.js
@@ -4,6 +4,12 @@ const data = [
   new Tour({ id: 121, name: "Hood River", price: 122.9 }),
   new Tour({ id: 122, name: "Gregon Coast", price: 129.9 }),
 ];
-exports.tourController = {
-  getData: () => data,
-};
+class TourController {
+  data;
+  getData = () => this.data;
+  constructor(data) {
+    this.data = data;
+  }
+}
+const tourController = new TourController(data);
+exports.tourController = tourController;
--- Cap_6/lib/__tests__/postApi.test.js
+++ Cap_6/lib/__tests__/postApi.test.js
@@ -1,29 +1,41 @@
 const { Tour } = require("../../api/model/tour");
 const { tourController } = require("../../api/tours");
-describe("postApi", () => {
-  const setData = (data) => {
-    if (!(data instanceof Tour)) {
+const data = tourController.getData();
+class TourControllerSpy {
+  data = new Array(Tour);
+  constructor({ data }) {
+    this.data = data;
+  }
+
+  getData = () => this.data;
+  setData = (newTour) => {
+    if (!(newTour instanceof Tour)) {
       throw new Error(
         "Não foi possível cadastrar o novo tour, entrada inválida"
       );
     }
-    if (tourController.getData().includes(data)) {
+    if (tourController.getData().includes(newTour)) {
       throw new Error("Tour já existe");
     }
   };
+}
+const makeTour = () => new TourControllerSpy(data);
+describe("postApi", () => {
   it("O método deve lançar uma exceção caso o tipo de dado não seja adequado", () => {
+    const tourControllerSpy = makeTour();
     const newData = {};
     const mock = () => {
-      setData(newData);
+      tourControllerSpy.setData(newData);
     };
     expect(mock).toThrow(
       new Error("Não foi possível cadastrar o novo tour, entrada inválida")
     );
   });
   it("O método deve lançar uma exceção caso o dado já exista no banco de dados", () => {
+    const tourControllerSpy = makeTour();
     const newData = tourController.getData()[0];
     const mock = () => {
-      setData(newData);
+      tourControllerSpy.setData(newData);
     };
     expect(mock).toThrow(new Error("Tour já existe"));
   });
