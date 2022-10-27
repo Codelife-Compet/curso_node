8b3c1ca1a97b6f56c3e7805912905c07808752c6
Francisco <franciscoabreu1408@gmail.com>
2022-10-27 12:10:12 -0300

    Export handlers created

--- Cap_6/codelife.js
+++ Cap_6/codelife.js
@@ -32,76 +32,24 @@ app.get("/", handlers.home);
 app.get("/about", handlers.about);
 app.get("/cookieSort", handlers.cookieSort);
 app.get("/thank-you", handlers.thanks);
-app.get("/greeting", (req, res) => {
-  if (req.cookie && !req.cookies.userId) {
-    res.cookie("userId", "123");
-  }
-  if (req.session && !req.session.username) {
-    req.session.username = "Pablo Emilio Escobar";
-  }
-  const message = "Olá estimado programador!";
-  const style = req.query.style || "color:inherit";
-  const userId = req.cookies.userId || "userId nao encontrado";
-  const username = req.session.username || "Usuario nao encontrado";
-  res.render("greeting", { message, style, userId, username });
-});
-app.get("/robustForm", (req, res) => {
-  res.render("robustForm");
-});
-app.get("/custom-layout", (req, res) =>
-  res.render("custom-layout", { layout: "custom" })
-);
-app.get("/no-layout", (req, res) => res.render("no-layout", { layout: null }));
-
-app.get("/text", (req, res) => {
-  res.type("text/plain");
-  res.send("this is a teste");
-});
-app.post("/basicForm/process", (req, res) => {
-  const email = req.body.email;
-  const username = req.body.username;
-  console.log(` Nome de usuário :${username}, email : ${email}`);
-  res.redirect(303, "/thank-you");
-});
-
-app.get("/basicForm", (req, res) => {
-  res.render("basicForm");
-});
-
-app.post("/robustForm/process", (req, res) => {
-  const email = req.body.email;
-  const username = req.body.username;
-
-  try {
-    // here's where we would try to save contact to database or other
-    // persistence mechanism...for now, we'll just simulate an error
-    if (req.body.simulateError) throw new Error("Error saving contact!");
-    console.log(`contact from ${username} <${email}>`);
-    res.format({
-      "text/html": () => res.redirect(303, "/thank-you"),
-      "application/json": () => res.json({ success: true }),
-    });
-  } catch (err) {
-    // here's where we would handle any persistence failures
-    console.error(`error processing contact from ${username} + <${email}>`);
-    res.format({
-      "text/html": () => res.redirect(303, "/contact-error"),
-      "application/json": () =>
-        res.status(500).json({
-          error: "error saving contact information",
-        }),
-    });
-  }
-});
-app.get("/robustForm", (req, res) => {
-  res.render("robustForm");
-});
-app.get("/contact-error", (req, res) => {
-  res.render("contact-error");
-});
-
+app.get("/text", handlers.text);
+app.get("/custom-layout", handlers.customLayout);
+app.get("/no-layout", handlers.noLayout);
+app.get("/greeting", handlers.greeting);
+app.get("/contact-error", handlers.contactError);
 app.use(handlers.notFound);
 app.use(handlers.serverError);
+
+// Basic Form
+app.post("/basicForm/process", handlers.basicFormProcess);
+
+app.get("/basicForm", handlers.basicForm);
+
+// Robust Form
+
+app.post("/robustForm/process", handlers.robustFormProcess);
+
+app.get("/robustForm", handlers.robustForm);
 
 // Iniciando o servidor
 require.main === module
--- Cap_6/lib/handlers.js
+++ Cap_6/lib/handlers.js
@@ -24,3 +24,76 @@ exports.serverError = (_err, _req, res, _next) => {
 exports.thanks = (req, res) => {
   res.render("thank-you");
 };
+
+exports.text = (req, res) => {
+  res.type("text/plain");
+  res.send("this is a teste");
+};
+
+exports.customLayout = (req, res) =>
+  res.render("custom-layout", { layout: "custom" });
+
+exports.noLayout = (req, res) => res.render("no-layout", { layout: "null" });
+
+exports.greeting = (req, res) => {
+  if (req.cookie && !req.cookies.userId) {
+    res.cookie("userId", "123");
+  }
+  if (req.session && !req.session.username) {
+    req.session.username = "Francisco Abreu";
+  }
+  const message = "Olá estimado programador!";
+  const style = req.query.style || "color:inherit";
+  const userId = req.cookies.userId || "userId nao encontrado";
+  const username = req.session.username || "Usuario nao encontrado";
+  res.render("greeting", { message, style, userId, username });
+};
+
+exports.contactError = (req, res) => {
+  res.render("contact-error");
+};
+
+// Basic Form
+
+exports.basicFormProcess = (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  console.log(` Nome de usuário :${username}, email : ${email}`);
+  res.redirect(303, "/thank-you");
+};
+
+exports.basicForm = (req, res) => {
+  res.render("basicForm");
+};
+
+// Robust Form
+
+exports.robustFormProcess = (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+
+  try {
+    // here's where we would try to save contact to database or other
+    // persistence mechanism...for now, we'll just simulate an error
+    if (req.body.simulateError) throw new Error("Error saving contact!");
+    console.log(`contact from ${username} <${email}>`);
+    res.format({
+      "text/html": () => res.redirect(303, "/thank-you"),
+      "application/json": () => res.json({ success: true }),
+    });
+  } catch (err) {
+    // here's where we would handle any persistence failures
+    console.error(`error processing contact from ${username} + <${email}>`);
+    res.format({
+      "text/html": () => res.redirect(303, "/contact-error"),
+      "application/json": () =>
+        res.status(500).json({
+          error: "error saving contact information",
+        }),
+    });
+  }
+};
+
+exports.robustForm = (req, res) => {
+  res.render("robustForm");
+};
